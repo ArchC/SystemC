b2f4b4df0902bc000df817727381630c239db21c
Tiago Rezende Campos Falcao <tiago@tiagofalcao.com>
2016-11-28 22:28:50 -0200

    Use preprocessor to mark unused arguments

--- examples/sysc/2.1/specialized_signals/scx_signal_signed.h
+++ examples/sysc/2.1/specialized_signals/scx_signal_signed.h
@@ -711,7 +711,7 @@ inline sc_dt::sc_signed sc_signal<sc_dt::sc_bigint<W> >::read_part(
 
 SC_TEMPLATE // Register a port for write checking.
 inline void sc_signal<sc_dt::sc_bigint<W> >::register_port( 
-	sc_port_base& port_, const char* if_typename_ )
+	__attribute__((unused)) sc_port_base& port_, __attribute__((unused)) const char* if_typename_ )
 {
 #       ifdef DEBUG_SYSTEMC
 		std::string nm( if_typename_ );
@@ -724,8 +724,6 @@ inline void sc_signal<sc_dt::sc_bigint<W> >::register_port(
 			}
 			m_output_p = &port_;
 		}
-#       else
-            if ( &port_ && if_typename_ ) {} // Silence unused args warning.
 #       endif
 }
 
